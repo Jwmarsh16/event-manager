b30175ab5a3c0800c498f2a5775ee7ebd7bd2dad
Jonathan Marshall <jwmarsh16@gmail.com>
2024-10-17 18:31:41 -0500

    initial commit

---
+++ server/migrations/versions/aa4c56e09bec_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: aa4c56e09bec
+Revises: 
+Create Date: 2024-10-17 18:31:00.791831
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'aa4c56e09bec'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
