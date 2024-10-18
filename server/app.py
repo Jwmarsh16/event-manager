2b1deb7c82e07fb3937425c6e2c499607682f028
Jonathan Marshall <jwmarsh16@gmail.com>
2024-10-17 20:51:58 -0500

    added class UserList to app.py to GET list of Users

--- server/app.py
+++ server/app.py
@@ -6,5 +6,18 @@ from config import app, db, api
 from datetime import datetime
 import json
 
+class UserList(Resource):
+    def get(self):
+        limit = request.args.get('limit', 30)  # Default to 30 if no limit is provided
+        query = request.args.get('q', '')
+
+        if query:
+            users = User.query.filter(User.username.ilike(f"%{query}%")).limit(limit).all()
+        else:
+            users = User.query.limit(limit).all()
+
+        return [user.to_dict() for user in users], 200
+
+
 if __name__ == "__main__":
   app.run(port=5555, debug=True)
